a2b719ced6cd5b652e1816a9a7eaae461416d3e2
Alvin <alvindejesus88@gmail.com>
2021-05-28 00:50:44 -0400

    Add project files.

---
+++ DAL/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Alvin_P1_AP2.DAL
+{
+    public class Contexto : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data source=Data\Parcial1.db");
+        }
+    }
+}
