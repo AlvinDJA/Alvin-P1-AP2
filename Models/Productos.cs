c2059c6c4aca89fc3ad2c04f036ed3192e4dbd2e
Alvin <alvindejesus88@gmail.com>
2021-06-02 18:24:49 -0400

    Models (Productos)

---
+++ Models/Productos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Alvin_P1_AP2.Models
+{
+    public class Productos
+    {
+        [Key]
+        public int ProductoId { get; set; }
+
+        [Required(ErrorMessage = "Inserte la descripción del producto")]
+        public string Descripcion { get; set; }
+
+        public float Existencia { get; set; } = 0;
+
+        [Range(minimum: 0, maximum: 50000000, ErrorMessage = "Inserte valor válido")]
+        public float Costo { get; set; } = 0;
+
+        [Range(minimum: 0, maximum: 50000000, ErrorMessage = "Inserte valor válido")]
+        public float ValorInventario { get; set; } = 0;
+    }
+}
