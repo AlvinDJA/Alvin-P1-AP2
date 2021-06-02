d017c8e43060ed43be1fc4a4d6371880cfb5e70f
Alvin <alvindejesus88@gmail.com>
2021-06-02 18:43:54 -0400

    BLL (ProductosBLL)

---
+++ BLL/ProductosBLL.cs
@@ -0,0 +1,147 @@
+﻿using Alvin_P1_AP2.DAL;
+using Alvin_P1_AP2.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Alvin_P1_AP2.BLL
+{
+    public class ProductosBLL
+    {
+        public static bool Guardar(Productos producto)
+        {
+            if (!Existe(producto.ProductoId))
+                return Insertar(producto);
+            else
+                return Modificar(producto);
+        }
+        public static bool Existe(int id)
+        {
+            Contexto contexto = new Contexto();
+            bool encontrado = false;
+            try
+            {
+                encontrado = contexto.Productos.Any(e => e.ProductoId == id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return encontrado;
+        }
+
+        private static bool Insertar(Productos producto)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+            try
+            {
+                contexto.Productos.Add(producto);
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+        public static bool Modificar(Productos producto)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+            try
+            {
+                contexto.Entry(producto).State = EntityState.Modified;
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+        public static bool Eliminar(int id)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+            try
+            {
+                var producto = contexto.Productos.Find(id);
+
+                if (producto != null)
+                {
+                    contexto.Productos.Remove(producto);
+                    paso = contexto.SaveChanges() > 0;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+
+        public static Productos Buscar(int id)
+        {
+            Contexto contexto = new Contexto();
+            Productos producto;
+
+            try
+            {
+                producto = contexto.Productos
+                    .Where(e => e.ProductoId == id)
+                    .FirstOrDefault();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return producto;
+        }
+        public static List<Productos> GetList(Expression<Func<Productos, bool>> producto)
+        {
+            List<Productos> Lista = new List<Productos>();
+            Contexto contexto = new Contexto();
+            try
+            {
+                Lista = contexto.Productos.Where(producto).ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return Lista;
+        }
+    }
+}
