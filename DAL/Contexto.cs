1a5c4443c99166daf3835d56eb753e870e81f0a4
Alvin <alvindejesus88@gmail.com>
2021-06-02 18:40:12 -0400

    DAL (Contexto)

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Alvin_P1_AP2.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +9,7 @@ namespace Alvin_P1_AP2.DAL
 {
     public class Contexto : DbContext
     {
+        public DbSet<Productos> Productos { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite(@"Data source=Data\Parcial1.db");
